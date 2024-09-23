5a01b5d6795aa5b8608dab482fe4db9aeda30894
ALUMNO <constanza.tapia23@inacapmail.cl>
2024-09-22 22:34:07 -0300

    uno

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+
+    }
+
+
+rootProject.name = "sociart"
+include(":app")
